dd4e81b64cc68e4814a93cd906b10b8c1752266a
i-am-new-blip <newaccount123321333@proton.me>
2025-07-16 14:07:40 -0300

    Update and rename install2 to install2.py
    some fixes

--- install2.py
+++ install2.py
@@ -1,15 +1,17 @@
 import os, sysconfig
-get, ocwd = globals()['get'], os.getcwd() # For readers | THIS EXISTS
+ocwd = os.getcwd() # For readers | THIS EXISTS
 #region setup
 os.chdir(sysconfig.get_paths()['purelib']) #great!
 if os.path.exists("pvenvgh"): exit()
 os.makedirs(os.path.join('pvenvgh','commands')); os.chdir("pvenvgh")
-with open('.version-control') as f: f.write(get('.version-control'))
+with open('.version-control','w') as f: f.write(get('.version-control'))
+with open('updater.py','w') as f: f.write(get('updater.py'))
 with open(os.path.join('commands','example.py'),'w') as f: f.write(get("commands/example.py"))
 with open('__init__.py') as f: f.write(get("main.py"))
 #endregion
 #region launch
+del sysconfig,get,request
 os.chdir(ocwd)
-import pvenvgh
-pvenvgh.main(get)
+del os,ocwd
+print("\x1b[2J\x1b[HPlease type out \"import pvenvgh as p; p.main()\"")
 #endregion
